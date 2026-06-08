09a81eafb32ebd7a67d9516f5964c7d9013f005f
halka9000stg <as.he.that.doth.serve@gmail.com>
2026-06-09 00:26:41 +0900

    add: CLIコマンド(hogcv)追加

---
+++ bin/hogcv.dart
@@ -0,0 +1,11 @@
+import "package:hognaverm/conv.dart";
+
+void main(List<String> args) {
+  final direction = args.first;
+  final target = args[2];
+  print(switch(direction){
+    decrypt => hognaverm.decode(target),
+    encrypt => hognaverm.encode(target),
+    _ => "\$ hogcv [decrypt | encrypt] <target-string>",
+  });
+}
