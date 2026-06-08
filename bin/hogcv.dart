420a9da943abe14f3f773f5872febcae4338cd52
halka9000stg <as.he.that.doth.serve@gmail.com>
2026-06-09 00:40:15 +0900

    fix: コマンドの引数の数を検査しわすれ

--- bin/hogcv.dart
+++ bin/hogcv.dart
@@ -1,11 +1,16 @@
 import "package:hognaverm/conv.dart";
 
 void main(List<String> args) {
+  final usage = "\$ hogcv [decrypt | encrypt] <target-string>";
+  if (args.length < 2) {
+    print(usage);
+    return;
+  }
   final direction = args.first;
   final target = args[2];
   print(switch(direction){
     "decrypt" => hognaverm.decode(target),
     "encrypt" => hognaverm.encode(target),
-    _ => "\$ hogcv [decrypt | encrypt] <target-string>",
+    _ => usage,
   });
 }
